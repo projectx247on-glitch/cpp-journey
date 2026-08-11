d656d53756bf33b210a2573d284261687a1b1a53
projectx247on-glitch <project.x.247.on@gmail.com>
2026-08-12 01:03:05 +0530

    Update day1.cpp

--- day1.cpp
+++ day1.cpp
@@ -12,54 +12,62 @@
 //     cout<< "loop ended";
 //        // return 0;
     
+// // }
+
+// in this i am trying to make something...
+// #include<iostream>
+// using namespace std;
+
+// int subgrade(string courses[], string grades[]){
+//     for(string course: courses){
+//        if(grade== "S"){
+//         return 10;
+//        }
+//        else if(grade== "A"){
+//         return 10;
+//        }
+//        else if(grade== "AB"){
+//         return 9;
+//        }
+//        else if(grade== "B"){
+//         return 8;
+//        }
+//        else if(grade== "BC"){
+//         return 7;
+//        }
+//        else if(grade== "C"){
+//         return 6;
+//        }
+//        else if(grade== "CD"){
+//         return 5;
+//        }
+//        else if(grade== "D"){
+//         return 4;
+//          }
+//        else{
+//         return 0;
+//        }
+//     }
+
 // }
-#include<iostream>
-using namespace std;
+// int main(){
+//   int grade;
+//     string grades[]= {"S","A","AB","B","BC","C","CD","D","F"};
+//     int courseNo;
+//     cout<< "enter course numbers ";
+//     cin>> courseNo;
+//     string courses[courseNo];
+//     for(course: courses){
+//         cout<< "enter course name ";
+//         cin>> courses;
+         
+//         cout<< "enter grade for "<< course<<endl;
+//         cin>>grade;
+//     }
+//     subgrade(grades);
+// cout<< "your CPI is:";
+// int CPI;
+// cout<< CPI;
 
-int subgrade(string courses[], string grades[]){
-    for(string course: courses){
-       if(grade== "S"){
-        return 10;
-       }
-       else if(grade== "A"){
-        return 10;
-       }
-       else if(grade== "AB"){
-        return 9;
-       }
-       else if(grade== "B"){
-        return 8;
-       }
-       else if(grade== "BC"){
-        return 7;
-       }
-       else if(grade== "C"){
-        return 6;
-       }
-       else if(grade== "CD"){
-        return 5;
-       }
-       else if(grade== "D"){
-        return 4;
-         }
-       else{
-        return 0;
-       }
-    }
-
-}
-int main(){
-  int grade;
-    string grades[]= {"S","A","AB","B","BC","C","CD","D","F"};
-    string courses[]= {"ISL", "BL_LAB", "CLP", "PPS", "BE", "CODE", "TCE"};
-    for(course: courses){
-        cout<< "enter grade for "<< course<<endl;
-        cin>>grade;
-    }
-    subgrade(grades);
-cout<< "your CPI is:";
-int CPI = (ISL*1 + BL_LAB*1.5 + CLP*4 + PPS*4.5 + BE*4 + CODE*4 + TCE*3)/21;
-cout<< CPI;
-
-return 0;
-}
+// return 0;
+// }
