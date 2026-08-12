e71615bb4c1ef955116047980e9e671a1716f700
projectx247on-glitch <project.x.247.on@gmail.com>
2026-08-13 00:50:22 +0530

    Update day3.cpp

--- day3.cpp
+++ day3.cpp
@@ -1,7 +1,28 @@
+//from brocode...
 #include<iostream>
-using namespace std;
-
+int search(int arr[], int size, int element);
 int main(){
-  
-  return 0;
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int index;
+    int myNum;
+    std::cout<<"enter element to search for:"<<'\n';
+    std::cin>>myNum;
+
+index = search(arr,size,myNum);
+if(index != -1){
+    std::cout<<"element found at index:"<<index<<'\n';
+    return 0;
+}
+std::cout<<"element not found"<<'\n';
+return 0;
+}
+
+int search(int arr[], int size, int element){
+    for(int i = 0; i<size; i++){
+        if(arr[i] == element){
+            return i;
+        }
+    }
+    return -1;
 }
