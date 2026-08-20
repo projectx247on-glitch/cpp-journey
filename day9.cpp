d50405442b33f5e8416382ec1832400b9e60191a
projectx247on-glitch <project.x.247.on@gmail.com>
2026-08-20 20:29:59 +0530

    Comment out main function in day9.cpp
    Commented out the main function and related code.

---
+++ day9.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+// int main(){
+//     int *ptr = nullptr;
+//     int x = 123;
+//     //ptr = &x;
+
+//     if(ptr== nullptr){
+//         std::cout<< "value isn't assigned";
+//     }
+//     else
+//     std::cout<< "value is assigned";
+//     return 0;
+// }
